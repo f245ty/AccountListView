57cdf93d16c05c667316a41622ea38f3d2d66fe3
田倉 芳明 <y-takura@ds.iiji.jp>
2020-03-09 18:25:26 +0900

    クレデンシャル付きAPI呼び出しに変更

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,99 +1,105 @@
+import AWS from 'aws-sdk';
+var apigClientFactory = require('aws-api-gateway-client').default;
 
-function getQeryString(state, csv_flag) {
+AWS.config.region = 'ap-northeast-1';
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'ap-northeast-1:9cd11c18-7668-4ea3-8427-40a8aed8ec94',
+    IdentityId: ''
+});
 
-    var sort_string;
-    // リストの中のソートキーを取得
-    if (!state.sort) {
-        sort_string = "";
-    } else {
-        sort_string = Object.keys(state.sort)
-    }
 
-    // query_stringを作成
-    var query_string = '?id=' + state.id
-        + '&sort=' + sort_string
-        + '&order=' + state.order
-        + '&page=' + state.page
-        + '&rows=' + (csv_flag ? 0 : state.rows)
-    console.log("query_string :" + query_string);
-
-    return query_string;
-}
 
 async function fetchData(state, csv_flag = false) {
     
-    var query_string = getQeryString(state, csv_flag)
-    
     // 検索モードによってAPIを変更する
-    let url = "";
-    if (state.type === "owner") {
-        url = 'https://k8bto0c6d5.execute-api.ap-northeast-1.amazonaws.com/prototype/owner';
-    } else if (state.type === "user") {
-        url = 'https://k8bto0c6d5.execute-api.ap-northeast-1.amazonaws.com/prototype/user';
+    let url = "https://k8bto0c6d5.execute-api.ap-northeast-1.amazonaws.com/prototype/";
+    if (state.type === "owner") url = url + 'owner';
+    else if (state.type === "user") url = url + 'user';
+
+    // CSV 出力指定の時は行数を 0 で指定
+    var localstate = {}
+    for( let key in state) localstate[key] = (key == 'rows' && csv_flag == true ) ? 0 : state[key]
+
+
+    // クレデンシャルの取得
+    //【TODO】切り出し
+    AWS.config.credentials.get((err) => {
+        console.log(AWS.config.credentials);
+        if(!err)
+        {
+            console.log('できた');
+            console.log(AWS.config.credentials);
+        }
+        else
+        {
+            console.log(err)
+        }
+    });
+    
+    var config = {
+        invokeUrl: url,
+        accessKey: AWS.config.credentials.accessKeyId,
+        secretKey: AWS.config.credentials.secretAccessKey,
+        sessionToken: AWS.config.credentials.sessionToken,
+        region: 'ap-northeast-1'
     }
 
-    // responseからデータ取得
-    return fetch(url + query_string, { mode: 'cors' })
-        .then((response) => {
-            return response.json();
-        })
-        .then((myJson) => {
-            // console.log(myJson)
-            if (myJson.items) {
-                if (myJson.items.message) {
-                    // console.info("Not found data. : " + myJson.items.message);
-                } else {
-                    // console.info("Success!");
-                }
-                return modeling(myJson, state);
-            } else {
-                // console.error("Failed to search. : " + myJson.errorType);
-                state.items = [];
-                return state;
-            }
 
-        });
+    var apigClient = apigClientFactory.newClient(config);
+
+    // ここまで切り出し
+
+    var pathParams = {};
+    var pathTemplate = '';
+    var method = 'GET';
+    var additionalParams = {
+        queryParams: localstate
+    }
+    var body = {}
+    
+    return apigClient.invokeApi(pathParams, pathTemplate, method, additionalParams, body)
+    .then(function(result){
+        return modeling(result.data, state);
+        //This is where you would put a success callback
+    }).catch( function(result){
+        //This is where you would put an error callback
+        state.items = [];
+        return state;
+    });
 }
 
 
 // Dynamo の JSON から内部用 JSON リストに成形
 function modeling(data, state) {
-    if (!data.items.message) {
-        var items = data.items;
+    var items = data.items;
 
-        var rows = [];
-        var count = 0;
-        items.forEach(item => {
-            var col = {};
-            col['#'] = ++count;
-            for (var value in item) {
-                if (item[value] === "1") {
-                    col[value] = "●";
-                } else if (item[value] === "0") {
-                    col[value] = "";
-                } else {
-                    col[value] = item[value];
-                }
+    var rows = [];
+    var count = 0;
+    items.forEach(item => {
+        var col = {};
+        col['#'] = ++count;
+        for (var value in item) {
+            if (item[value] === "1") {
+                col[value] = "●";
+            } else if (item[value] === "0") {
+                col[value] = "";
+            } else {
+                col[value] = item[value];
             }
-            rows.push(col);
-        });
-        // console.log("Received");
+        }
+        rows.push(col);
+    });
 
-        // 検索時のqueryと返ってきたqueryをマージ
-        let result = data.query;
-        result.type = state.type;
-        result.id = data.query.id;
-        result.sort = state.sort;
-        result.order = state.order;
-        result.items = rows;
-        // console.log(result);
-        return result;
-
-    } else {
-        data.items = [];
-        // console.log("rows = 0 : "+data);
-        return data;
-    }
+    // 検索時のqueryと返ってきたqueryをマージ
+    let result = data.query;
+    result.type = state.type;
+    result.id = data.query.id;
+    result.sort = state.sort;
+    result.order = state.order;
+    result.items = rows;
+    // console.log(result);
+    console.log(result)
+    return result;
 }
 
 export default fetchData;
