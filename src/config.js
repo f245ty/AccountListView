318c07cd499f2f5219a38db9ca8bf93323c0ec10
田倉 芳明 <y-takura@ds.iiji.jp>
2020-03-16 13:43:56 +0900

    閲覧権限が表示されない問題に、設定ファイルの誤りを修正

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export const HEADER_LABEL = {
     "folder_path": "フォルダパス",
     "permission": "権限",
     "owner_name": "管理者名",
-    "p_read": "閲覧権限",
+    "p_view": "閲覧権限",
     "p_download": "ダウンロード権限",
     "p_upload": "アップロード権限",
     "p_admin": "管理権限",
@@ -18,12 +18,12 @@ export const HEADER_LABEL = {
 
 
 // 表示ラベルの順番、ラベルの表示、非表示の設定
-export const PERMISSION_LABELS = ['p_read', 'p_upload', 'p_download', 'p_delete', 'p_admin']
-export const OWNER_LABELS = ['#', 'owner_name', 'folder_path', 'user_email', 'user_name']
-export const USER_LABELS = ['#', 'folder_path', 'owner_name']
-export const PERMISSION_LABELS_CSV = ['#', 'p_read', 'p_upload', 'p_download', 'p_delete', 'p_admin']
+export const PERMISSION_LABELS     = ['p_view', 'p_upload', 'p_download', 'p_delete', 'p_admin']
+export const PERMISSION_LABELS_CSV = ['p_view', 'p_upload', 'p_download', 'p_delete', 'p_admin']
+export const OWNER_LABELS     = ['#', 'owner_name', 'folder_path', 'user_email', 'user_name']
 export const OWNER_LABELS_CSV = ['#', 'folder_path', 'owner_name', 'user_email', 'user_name']
-export const USER_LABELS_CSV = ['#', 'folder_path', 'owner_name']
+export const USER_LABELS     =  ['#', 'folder_path', 'owner_name']
+export const USER_LABELS_CSV =  ['#', 'folder_path', 'owner_name']
 
 // 1ページあたりのページ数のデフォルト
 export const DEFAULT_ROWS_PAR_PAGE = 5
